f5ae0530d5dc66b814ee58c0fc08ee00402cb13a
Ricardo Leme <ricardo.leme@gmail.com>
2022-11-01 11:08:03 -0300

    Ajustes

--- js/firebase.js
+++ js/firebase.js
@@ -3,14 +3,17 @@
 ***********************************************/
 
 // Cole as informações do seu RealTime Database do Firebase abaixo:
-var firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+const firebaseConfig = {
+  apiKey: "AIzaSyCJoQLYXRkE2pScMsDpPZWGFWQVnR3GyMs",
+  authDomain: "leme-fatecitu-gti.firebaseapp.com",
+  databaseURL: "https://leme-fatecitu-gti-default-rtdb.firebaseio.com",
+  projectId: "leme-fatecitu-gti",
+  storageBucket: "leme-fatecitu-gti.appspot.com",
+  messagingSenderId: "394057380184",
+  appId: "1:394057380184:web:9f9755997a28697b41b52d",
+  measurementId: "G-5ZVJSVZ7XZ"
 };
+
 
 /*
 * Nas regras do Realtime Database, informe:
