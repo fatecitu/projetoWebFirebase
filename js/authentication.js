a37a3a2df0cc8e263bf1b477e03defde002a5c8d
Ricardo Leme <ricardo.leme@gmail.com>
2022-11-08 22:22:42 -0300

    Aula 08/11

--- js/authentication.js
+++ js/authentication.js
@@ -1,7 +1,6 @@
 //baseURL
-const baseURL = window.location.hostname.includes('127.0.0.1')
-? 'http://127.0.0.1:5500'
-: 'https://fatecitu-pi.firebaseapp.com'
+const baseURL = 'http://127.0.0.1:5500'
+
 
 /**
  * novoUsuario.
--- js/dados.js
+++ js/dados.js
@@ -6,16 +6,17 @@
  * @return {null} - Snapshot atualizado dos dados
  */
 
-function salvar(event, collection){
+function salvar(event, collection) {
     event.preventDefault() // evita que o formulário seja recarregado
     //Verificando os campos obrigatórios
-    if(document.getElementById('nome').value === '') { alert('⚠ É obrigatório informar o nome!')}
-    else if(document.getElementById('email').value === '') { alert('⚠ É obrigatório informar o email!')}
-    else if(document.getElementById('nascimento').value === '') { alert('⚠ É obrigatório informar a data de Nascimento!')}
-    else {incluir(event, collection)}
+    if (document.getElementById('nome').value === '') { alert('⚠ É obrigatório informar o nome!') }
+    else if (document.getElementById('email').value === '') { alert('⚠ É obrigatório informar o email!') }
+    else if (document.getElementById('nascimento').value === '') { alert('⚠ É obrigatório informar a data de Nascimento!') }
+    else if (document.getElementById('id').value !== '') { alterar(event, collection) }
+    else { incluir(event, collection) }
 }
 
-function incluir(event, collection){
+function incluir(event, collection) {
     event.preventDefault() // evita que o formulário seja recarregado
     //Obtendo os campos do formulário
     const form = document.forms[0]
@@ -25,15 +26,15 @@ function incluir(event, collection){
     //console.log(`Os dados são:`)
     //console.log(values)
     //O retorno é uma Promise (promessa)
-   return firebase.database().ref(collection).push(values)
-    .then(()=> {
-        alert('✔ Registro cadastrado com sucesso!')
-        document.getElementById('formCadastro').reset() //limpar o formulário
-    })
-    .catch(error => {
-        console.error(`Ocorreu um erro: ${error.code}-${error.message}`)
-        alert(`❌ Falha ao incluir: ${error.message}`)
-    })
+    return firebase.database().ref(collection).push(values)
+        .then(() => {
+            alert('✔ Registro cadastrado com sucesso!')
+            document.getElementById('formCadastro').reset() //limpar o formulário
+        })
+        .catch(error => {
+            console.error(`Ocorreu um erro: ${error.code}-${error.message}`)
+            alert(`❌ Falha ao incluir: ${error.message}`)
+        })
 }
 
 /**
@@ -42,7 +43,7 @@ function incluir(event, collection){
  * @param {string} collection - Nome da Collection no Firebase
  * @return {object} - Uma tabela com os dados obtidos
  */
-function obtemDados(collection){
+function obtemDados(collection) {
     var tabela = document.getElementById('tabelaDados')
     firebase.database().ref(collection).on('value', (snapshot) => {
         tabela.innerHTML = ''
@@ -67,8 +68,8 @@ function obtemDados(collection){
             novalinha.insertCell().textContent = item.val().email
             novalinha.insertCell().textContent = item.val().sexo
             novalinha.insertCell().textContent = item.val().salario
-            novalinha.insertCell().innerHTML = 
-            `
+            novalinha.insertCell().innerHTML =
+                `
             <button class ='btn btn-danger' title='Remove o registro corrente' onclick=remover('${db}','${id}')>🗑 Excluir </button>
             <button class ='btn btn-warning' title='Edita o registro corrente' onclick=carregaDadosAlteracao('${db}','${id}')>✏ Editar </button>
             `
@@ -90,7 +91,7 @@ function obtemDados(collection){
  * @param {string} collection - Nome da Collection no Firebase
  * @return {string} - Texto com o total de registros
 * */
-function totalRegistros(collection){
+function totalRegistros(collection) {
     var retorno = '...'
     firebase.database().ref(collection).on('value', (snapshot) => {
         if (snapshot.numChildren() === 0) {
@@ -100,4 +101,71 @@ function totalRegistros(collection){
         }
     })
     return retorno
+}
+
+/**
+ * remover
+ * Remove os dados da collection a partir do id informado
+ * @param {string} db - Nome da collection no Firebase
+ * @param {integer} id - Id do registro no Firebase
+ * @return {null} - Snapshot atualizado dos dados
+ */
+function remover(db, id) {
+    //Iremos confirmar com o usuário
+    if (window.confirm('🔴Confirma a exclusão do registro?')) {
+        let dadoExclusao = firebase.database().ref().child(db + '/' + id)
+        dadoExclusao.remove()
+            .then(() => {
+                alert('✅Registro removido com sucesso!')
+            })
+            .catch(error => {
+                alert('❌Falha ao excluir: ' + error.message)
+            })
+    }
+}
+
+function carregaDadosAlteracao(db, id) {
+    firebase.database().ref(db).on('value', (snapshot) => {
+        snapshot.forEach(item => {
+            if (item.ref.path.pieces_[1] === id) {
+                document.getElementById('id').value = item.ref.path.pieces_[1]
+                document.getElementById('nome').value = item.val().nome
+                document.getElementById('email').value = item.val().email
+                document.getElementById('nascimento').value = item.val().nascimento
+                document.getElementById('salario').value = item.val().salario
+                if (item.val().sexo === 'Masculino') {
+                    document.getElementById('sexoM').checked = true
+                } else {
+                    document.getElementById('sexoF').checked = true
+                }
+            }
+        })
+    })
+}
+
+function alterar(event, collection) {
+    event.preventDefault()
+    //Obtendo os campos do formulário
+    const form = document.forms[0];
+    const data = new FormData(form);
+    //Obtendo os valores dos campos
+    const values = Object.fromEntries(data.entries());
+    console.log(values)
+    //Enviando os dados dos campos para o Firebase
+    return firebase.database().ref().child(collection + '/' + values.id).update({
+        nome: values.nome,
+        email: values.email,
+        sexo: values.sexo,
+        nascimento: values.nascimento,
+        salario: values.salario
+    })
+        .then(() => {
+            alert('✅ Registro alterado com sucesso!')
+            document.getElementById('formCadastro').reset()
+        })
+        .catch(error => {
+            console.log(error.code)
+            console.log(error.message)
+            alert('❌ Falha ao alterar: ' + error.message)
+        })
 }
